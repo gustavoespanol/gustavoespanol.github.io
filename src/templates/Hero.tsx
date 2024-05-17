18cc513ba4ce1c3cb0817a0c6def4843ca6b2852
luisarbezerra <luisarbezerra@gmail.com>
2024-05-17 11:20:06 +0200

    feat: visual adjusts

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -5,36 +5,32 @@ import { useTranslations } from 'next-intl';
 import { buttonVariants } from '@/components/ui/button';
 import { CenteredHero } from '@/features/landing/CenteredHero';
 import { Section } from '@/features/landing/Section';
+import { cn } from '@/utils/Helpers';
 
 const Hero = () => {
   const t = useTranslations('Hero');
 
   return (
-    <Section className="py-20 pb-16 md:py-32 md:pb-24">
-      <CenteredHero
-        title={t.rich('title', {
-          important: (chunks) => (
-            <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
-              {chunks}
-            </span>
-          ),
-        })}
-        description={t('description')}
-        buttons={
-          <>
-            <a className={buttonVariants({ size: 'lg' })} href="#method">
+    <div className={cn('w-full bg-secondary')}>
+      <Section className="py-20 pb-16 md:py-32 md:pb-24">
+        <CenteredHero
+          title={t.rich('title', {
+            important: (chunks) => (
+              <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+                {chunks}
+              </span>
+            ),
+          })}
+          description={t('description')}
+          buttons={
+            <a className={buttonVariants({ size: 'lg' })} href="#contact">
               <RocketIcon className="mr-2 size-5" />
-              {t('primary_button')}
+              {t('button')}
             </a>
-
-            <a className={buttonVariants({ variant: 'outline', size: 'lg' })} href="#contact">
-              <EnvelopeIcon className="mr-2 size-5" />
-              {t('secondary_button')}
-            </a>
-          </>
-        }
-      />
-    </Section>
+          }
+        />
+      </Section>
+    </div>
   );
 };
 
